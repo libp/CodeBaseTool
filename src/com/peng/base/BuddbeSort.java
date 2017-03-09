f9a05e2dab069926f09493d78a37400d912aec6f
lbp <924701358@qq.com>
2017-03-09 18:38:55 +0800

    增加冒泡排序算法

--- src/com/peng/base/BuddbeSort.java
+++ src/com/peng/base/BuddbeSort.java
@@ -1,4 +1,7 @@
 package com.peng.base;
+
+import java.util.Iterator;
+
 /** 
  * @author Peng
  * @Email 924701358@qq.com
@@ -6,8 +9,30 @@ package com.peng.base;
  * @version 1.0 
  * @return  
  */
-
+/*
+ * 冒泡排序，就是一次一次比，把最小的放最前面，或最大的放最后面
+ */
 public class BuddbeSort {
-
+	public static void main(String[] args) {
+		int[] arr = {3,2,6,8,2,9,5,4,1};
+		System.out.println("排序前数组为：");
+		for( int num : arr ){
+			System.out.print(num+" ");
+		}
+		for(int i = 0; i < arr.length; i++){
+			for (int j = 0; j < arr.length-i-1; j++) {
+				if(arr[j]>arr[j+1]){
+					int tmp = arr[j];
+					arr[j] = arr[j+1];
+					arr[j+1] = tmp;
+				}
+			}
+		}
+		System.out.println();
+		System.out.println("排序后的数组为：");
+		for( int num : arr){
+			System.out.print(num + " ");
+		}
+	}
 }
 
