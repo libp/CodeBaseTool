b8596c945e9e02dcba2c1b99e71967d3ef2b9cf7
lbp <924701358@qq.com>
2017-04-30 21:39:03 +0800

    update

--- src/com/peng/baseKnow/SpiltString.java
+++ src/com/peng/baseKnow/SpiltString.java
@@ -1,4 +1,8 @@
 package com.peng.baseKnow;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /** 
  * @author Peng
  * @Email 924701358@qq.com
@@ -10,10 +14,22 @@ package com.peng.baseKnow;
 public class SpiltString {
 	public static void main(String asg[]) {
 		String name = "PIT-LE-LSR-VM-ptculgp14";
+		String name2 = "PITptculgp14";
+		String name3 = "PIT-SW-APS-AP2-全渠道网银退货系统";
 		String[] tStrings = name.split("-");
+		String[] tStrings2 = name2.split("-");
 		System.out.println(tStrings[4]);
 		System.out.println(tStrings.length);
+		System.out.println(name3.substring(15));
 		System.out.println("ok");
+		
+		
+		
+		Map<String, String> map = new HashMap<String, String>();
+		map.put("id", "");
+		map.put("groupname", "dd");
+		map.put("id", "2222");
+		System.out.println(map.get("id"));
 	}
 }
 
